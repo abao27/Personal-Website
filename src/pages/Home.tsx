ed3b8dcea3be54943f8436d74f667417e311b74f
abao27 <alanb@berkeley.edu>
2025-08-19 12:29:12 -0700

    fixed background file

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 import { ArrowRight, Download, Github, Linkedin } from "lucide-react";
-import heroImage from "@/assets/hero-bg.jpg";
+import heroImage from "@/assets/homepage_bg.jpg";
 
 const Home = () => {
   return (
